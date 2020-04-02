dfe4ca5fe6178ac25eb3eee0ffead388fb7818bb
Lisa <lisa.rapinchuk@gmail.com>
2020-04-02 22:29:16 +0300

    feat: add services

--- src/by/epam/library/services/BookService.java
+++ src/by/epam/library/services/BookService.java
@@ -1,5 +1,58 @@
 package by.epam.library.services;
 
+import by.epam.library.entity.Author;
+import by.epam.library.entity.Book;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class BookService {
-    public
-}
+    public boolean containBigBook(Stream<Book> books) {
+        return books.anyMatch(book -> book.getNumberOfPages() > 200);
+    }
+
+    public Stream<Book> findMaxBook(Stream<Book> books) {
+        return books.max(Comparator.comparingInt(Book::getNumberOfPages)).stream();
+    }
+
+    public Stream<Book> findMinBook(Stream<Book> books) {
+        return books.min(Comparator.comparingInt(Book::getNumberOfPages)).stream();
+    }
+
+    public Stream<Book> filterSingleAuthorBook(Stream<Book> books) {
+        return books.filter(book -> book.getAuthors().size() == 1);
+    }
+
+    public Stream<Book> sortBooksByPages(Stream<Book> books) {
+        return books.sorted(Comparator.comparingInt(Book::getNumberOfPages));
+    }
+
+    public Stream<Book> sortBooksByTitles(Stream<Book> books) {
+        return books.sorted(Comparator.comparing(Book::getTitle));
+    }
+
+    public List<Author> getAuthorList (Stream<Book> books) {
+        List<Author> authors = new ArrayList<>();
+        books.forEach(book -> authors.addAll(book.getAuthors()));
+        return authors;
+    }
+
+    /*public void printAuthorList (List<Author> authors) {
+        authors.forEach(System.out::println);
+    }*/
+
+    public List<Author> getAuthorListWithoutDuplicates (Stream<Book> books) {
+        List<Author> authors = new ArrayList<>();
+        /*books.forEach(book -> book.getAuthors().forEach(author -> {
+            if(!authors.contains(author)) {
+                authors.add(author);
+            }
+        }));*/
+        Stream<Author> authorStream = getAuthorList(books).stream();
+        authorStream.distinct().forEach(authors::add);
+        return authors;
+    }
+ }
